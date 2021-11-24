f36ed76a1a9e4aeca7a6154927c3b28321f52642
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2021-11-24 20:08:56 +0700

    Delete Config.php

--- Config.php
+++
@@ -1,1 +0,0 @@
-<?php $host = "localhost"; $user = "root"; $pass = ""; $db = "reglog"; $table = "members"; $connect = mysql_connect($host,$user,$pass) or die("Gagal konek"); $pilih_db = mysql_select_db($db) or die("Database tidak ada"); $cookie_nama = $_COOKIE['nama']; ?>
