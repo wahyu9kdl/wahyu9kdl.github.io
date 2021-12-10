e6e49ea8fb54cb080b647cee3867425872f8c48c
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2021-12-10 10:54:56 +0700

    Create package.java

---
+++ java/package.java
@@ -0,0 +1,77 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author
+ */
+ .public class parseUrl {
+ public static void main(String[] args) throws MalformedURLException {
+ URL url = new URL("https://wahyu9kdl.github.io/DASHBOARD/PROJECTS/MIKROVER/Ping.html");
+ 
+ System.out.println("protocol: " + url.getProtocol());
+ System.out.println("authority: " + url.getAuthority());
+ System.out.println("host: " + url.getHost());
+ System.out.println("port: " + url.getPort());
+ System.out.println("path:" + url.getPath());
+ System.out.println("query: " + url.getQuery());
+ System.out.println("filename: " + url.getFile());
+ System.out.println("ref: " + url.getRef());
+ }
+ }
+ .public class parseUrl {
+ public static void main(String[] args) throws MalformedURLException {
+ URL url = new URL("https://wahyu9kdl.github.io/AwDev.html");
+ 
+ System.out.println("protocol: " + url.getProtocol());
+ System.out.println("authority: " + url.getAuthority());
+ System.out.println("host: " + url.getHost());
+ System.out.println("port: " + url.getPort());
+ System.out.println("path:" + url.getPath());
+ System.out.println("query: " + url.getQuery());
+ System.out.println("filename: " + url.getFile());
+ System.out.println("ref: " + url.getRef());
+ }
+ }
+ .public class parseUrl {
+ public static void main(String[] args) throws MalformedURLException {
+ URL url = new URL("https://wahyu9kdl.github.io/Ads.html");
+ 
+ System.out.println("protocol: " + url.getProtocol());
+ System.out.println("authority: " + url.getAuthority());
+ System.out.println("host: " + url.getHost());
+ System.out.println("port: " + url.getPort());
+ System.out.println("path:" + url.getPath());
+ System.out.println("query: " + url.getQuery());
+ System.out.println("filename: " + url.getFile());
+ System.out.println("ref: " + url.getRef());
+ }
+ }
+.public class parseUrl {
+    public static void main(String[] args) throws MalformedURLException {
+        URL url = new URL("https://www.alhikmah.my.id/p/sitemap-kata-kata-hikmah_3.html");
+
+        System.out.println("protocol: " + url.getProtocol());
+        System.out.println("authority: " + url.getAuthority());
+        System.out.println("host: " + url.getHost());
+        System.out.println("port: " + url.getPort());
+        System.out.println("path:" + url.getPath());
+        System.out.println("query: " + url.getQuery());
+        System.out.println("filename: " + url.getFile());
+        System.out.println("ref: " + url.getRef());
+    }
+}
+.public class parseUrl {
+    public static void main(String[] args) throws MalformedURLException {
+        URL url = new URL("https://www.alhikmah.my.id/p/daftar-campaign-affiliasi-website-kami.html");
+
+        System.out.println("protocol: " + url.getProtocol());
+        System.out.println("authority: " + url.getAuthority());
+        System.out.println("host: " + url.getHost());
+        System.out.println("port: " + url.getPort());
+        System.out.println("path:" + url.getPath());
+        System.out.println("query: " + url.getQuery());
+        System.out.println("filename: " + url.getFile());
+        System.out.println("ref: " + url.getRef());
+    }
+}
