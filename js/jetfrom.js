eccdae342a4e13d0e621c2a54829572e046d6bf7
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2021-12-29 22:48:31 +0700

    Update jetfrom.js

--- js/jetfrom.js
+++ js/jetfrom.js
@@ -1,4 +1,6 @@
-var JFL_211958944889075 = new JotformFeedback({
+<script src ="https://form.jotform.com/static/feedback2.js" type="text/javascript"></script><script type="text/javascript">
+        
+        var JFL_211958944889075 = new JotformFeedback({
           formId: '211958944889075',
           base: 'https://form.jotform.com/',
           windowTitle: 'Formulir',
@@ -10,7 +12,8 @@ var JFL_211958944889075 = new JotformFeedback({
            openOnLoad:true
           
         });
-    
+    </script>
+    <script language="JavaScript">
     
     var delayseconds = 5;
     
@@ -20,6 +23,6 @@ var JFL_211958944889075 = new JotformFeedback({
     
     setTimeout(function(){document.querySelector('.jt-dimmer').click();}, delayseconds*5000);
     
+    }
     
-    
-    });
+    </script>
