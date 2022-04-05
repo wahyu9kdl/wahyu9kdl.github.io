eec6b92e83ed0309c802994dce7900e64d45cdf5
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-04-05 18:34:07 +0700

    Create Profile.js

---
+++ vue/src/components/Profile.js
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Profile Page</h1>
+  </div>
+</template>
+<script>
+
+export default {
+
+  created() {
+    window.location.href = "https://wahyu9kdl.github.io";
+  },
+
+};
+</script>
