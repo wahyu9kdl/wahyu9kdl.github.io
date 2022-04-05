300f4fbf263cda4383bbd0d115b18453235008e7
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-04-05 15:10:49 +0700

    Create app.vue

---
+++ admin/src/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ul>
+      <li>Id : {{post.id}}</li>
+      <li>Title: {{post.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      post: ""
+    };
+  },
+  created() {
+    this.$http.get("https://jsonplaceholder.typicode.com/posts/1")
+    .then(res => {
+      this.post = res.body;
+    });
+  }
+};
+</script>
