976fe06579f916e31a8159c364b418023a7e2870
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-04-05 16:09:35 +0700

    Create home.vue

---
+++ vue/src/views/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <div class="content">
+      <List />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import List from "@/components/List.vue";
+export default {
+  name: "Home",
+  components: {
+    List
+  }
+};
+</script>
