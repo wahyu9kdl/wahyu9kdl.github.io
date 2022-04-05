e3b5801a0cac7493ab2b8832123a5b1fe616831a
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-04-05 18:41:28 +0700

    Update index.js

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Surah from "../views/Surah.vue";
+import About from "../views/About.vue";
+import Profile from "../views/Profile.vue";
+import Login from "../views/Login.vue";
+import Bloog from "../views/Bloog.vue";
 
 Vue.use(VueRouter);
